74973068d9ef23db20203e0b7ed92880632f7ebd
Antonio Mesquita Júnior <44874857+antoniomesquita09@users.noreply.github.com>
2023-11-08 16:09:23 -0300

    adjust code

--- motor_base.ino
+++ motor_base.ino
@@ -1,20 +1,3 @@
-
-/*
- Stepper Motor Control - one revolution
-
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
-
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
-
-
- Created 11 Mar. 2007
- Modified 30 Nov. 2009
- by Tom Igoe
-
- */
-
 #include <Stepper.h>
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
@@ -45,14 +28,7 @@ void loop() {
       myStepper.step(-stepsPerRevolution);
     }
   }
-  // step one revolution  in one direction:
-  // Serial.println("clockwise");
-  // myStepper.step(stepsPerRevolution);
-  // delay(500);
 
-  // step one revolution in the other direction:
-  // Serial.println("counterclockwise");
-  // myStepper.step(-stepsPerRevolution);
   // delay(500);
 }
 
