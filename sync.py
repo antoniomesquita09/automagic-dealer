a71579e2d0d0a08bfa1ffc5e55b64ca8ddc53cbf
João Nascimento <joao.nascimento@beegin.com.br>
2023-11-29 16:58:11 -0300

    load eeprom

--- sync.py
+++ sync.py
@@ -11,7 +11,7 @@ uri = "mongodb+srv://jpnas:iHBXPcSA5a3Rxph9@cluster0.482qyxh.mongodb.net/?retryW
 database_name = "card_games"
 
 # Serial Connection
-serial_port = 'dev/ttyACM0' # Substitua pelo seu dispositivo serial
+serial_port = '/dev/cu.usbmodem21301' # Substitua pelo seu dispositivo serial
 baud_rate = 9600  # Mesma taxa de baud do código Arduino
 
 def fetch_data():
@@ -33,8 +33,9 @@ def fetch_data():
     return data
 
 def send_data(serial_connection, data):
-    serialized_data = json.dumps(data)  # Convertendo a lista em uma string JSON
-    serial_connection.write(serialized_data.encode('utf-8'))
+    for game in data:
+        serial_connection.write(f'{game["name"]} {game["minPlayers"]} {game["maxPlayers"]}'.encode('utf-8'))
+
 
 def main():
     games_data = fetch_data()
@@ -42,12 +43,10 @@ def main():
     # Converte os dados para um formato adequado, se necessário
     # Por exemplo, uma simplificação ou serialização específica
 
-    print(games_data)
-
-    # with serial.Serial(serial_port, baud_rate) as ser:
-    #     time.sleep(2)  # Aguarda a conexão serial se estabilizar
-    #     send_data(ser, games_data)
-    #     print("Dados enviados com sucesso para o Arduino.")
+    with serial.Serial(serial_port, baud_rate) as ser:
+        time.sleep(2)  # Aguarda a conexão serial se estabilizar
+        send_data(ser, games_data)
+        print("Dados enviados com sucesso para o Arduino.")
 
 if __name__ == "__main__":
     main()
