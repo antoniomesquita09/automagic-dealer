cccd0d5209bd5b7637744c21b8effc91deebb9f4
Mark Duek <37782438+MarkDuek@users.noreply.github.com>
2023-12-13 23:03:36 -0300

    card detector

--- card_detector/Detector.py
+++ card_detector/Detector.py
@@ -4,6 +4,10 @@ import time
 import os
 import Cards
 import VideoStream
+import serial
+from time import sleep
+from threading import Thread
+ 
 
 ## Camera settings
 IM_WIDTH = 1280
@@ -18,7 +22,17 @@ RANK_HEIGHT = 125
 SUIT_WIDTH = 70
 SUIT_HEIGHT = 100
 
+# Serial
+serial_port = 'COM29'
+baud_rate = 9600
+
+# Cards
+cardsAllowed = [0]
+
 ### Structures to hold query card and train card information ###
+
+videostream = VideoStream.VideoStream((IM_WIDTH,IM_HEIGHT),FRAME_RATE,2,0).start()
+imageGlobal = videostream.read()
 
 class Query_card:
     """Structure to store information about query cards in the camera image."""
@@ -55,29 +69,61 @@ class Train_suits:
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
-videostream = VideoStream.VideoStream((IM_WIDTH,IM_HEIGHT),FRAME_RATE,2,0).start()
-time.sleep(1) # Give the camera time to warm up
-
 # Load the train rank and suit images
 path = os.path.dirname(os.path.abspath(__file__))
 train_ranks = Cards.load_ranks( path + '/Card_Imgs/')
 train_suits = Cards.load_suits( path + '/Card_Imgs/')
 
-cam_quit = 0 # Loop control variable
+# Serial Methods
+
+
+def listen_ard(ser):
+    #if ser.in_waiting > 0:
+    print('arduino received:')
+        
+    #global imageGlobal
+    while True:
+        #imageGlobal = videostream.read()
+        incoming_message = ser.readline().decode().strip()
+        print(incoming_message)
+        if incoming_message == "CARD_ALLOWED_REQUEST":
+            print('card request')
+            allowed = cardMode(imageGlobal)
+            send_data(ser, allowed)
+        elif incoming_message[0:14] == "EXCLUDED_CARDS":
+            print('cards allowed')
+            cardsAllowed = incoming_message[14:].split()
+            print(cardsAllowed)
+        else:
+            print('unknow msg')
+            
+            
+
+def send_data(ser, cardAllowed):
+    allowed = 'FALSE'
+    if cardAllowed:
+        allowed = 'TRUE'
+    try:
+        ser.write(f"CARD_ALLOWED_RESPONSE {allowed}\n".encode('utf-8'))
+        print(f"CARD_ALLOWED_RESPONSE {allowed}\n".encode('utf-8'))
+    except Exception as e:
+        print(f"Error sending data to Arduino: {e}")
 
 
 def detect(image):
     
     # Initialize new Query_card object
     qCard = Query_card()
+    # Pre-process camera image (gray, blur, and threshold it)
+    pre_proc = Cards.preprocess_image(image)
     
     crop = pre_proc[0:500, 0:200]
     crop = invert = cv2.bitwise_not(crop)
     cv2.imshow("cropp",crop)
     
     # Split in to top and bottom half (top shows rank, bottom shows suit)
-    Qrank = crop[40:200, 20:180]
-    Qsuit = crop[220:380, 20:180]
+    Qrank = crop[80:280, 20:180]
+    Qsuit = crop[300:460, 20:180]
     cv2.imshow("rank",Qrank)
     cv2.imshow("suit",Qsuit)
     
@@ -121,29 +167,145 @@ def detect(image):
     # Find the best rank and suit match for the card.
     cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff = Cards.match_card(cards[k],train_ranks,train_suits)
     
-    print(cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff)
+    best_rank_match_diff = 10000
+    best_suit_match_diff = 10000
+    best_rank_match_name = "Unknown"
+    best_suit_match_name = "Unknown"
+    
+    if (len(qCard.rank_img) != 0) and (len(qCard.suit_img) != 0):
+        
+        # Difference the query card rank image from each of the train rank images,
+        # and store the result with the least difference
+        for Trank in train_ranks:
+
+                diff_img = cv2.absdiff(qCard.rank_img, Trank.img)
+                rank_diff = int(np.sum(diff_img)/255)
+                
+                if rank_diff < best_rank_match_diff:
+                    best_rank_diff_img = diff_img
+                    best_rank_match_diff = rank_diff
+                    best_rank_name = Trank.name
+
+        # Same process with suit images
+        for Tsuit in train_suits:
+                
+                diff_img = cv2.absdiff(qCard.suit_img, Tsuit.img)
+                suit_diff = int(np.sum(diff_img)/255)
+                
+                if suit_diff < best_suit_match_diff:
+                    best_suit_diff_img = diff_img
+                    best_suit_match_diff = suit_diff
+                    best_suit_name = Tsuit.name
+
+    # Combine best rank match and best suit match to get query card's identity.
+    # If the best matches have too high of a difference value, card identity
+    # is still Unknown
+    if (best_rank_match_diff < 3500):
+        best_rank_match_name = best_rank_name
+
+    if (best_suit_match_diff < 3500):
+        best_suit_match_name = best_suit_name
+        
+        
+    #print(cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff)
+    
+    #print(best_rank_match_name, best_suit_match_name)
+    
+    return cards[k].best_rank_match, cards[k].best_suit_match
     #print(cards[k].best_rank_match,cards[k].best_suit_match)
     # Draw center point and match result on the image.
 
-    
-    
-# Begin capturing frames
-while cam_quit == 0:
 
-    # Grab frame from video stream
-    image = videostream.read()
-
-
-    # Pre-process camera image (gray, blur, and threshold it)
-    pre_proc = Cards.preprocess_image(image)
-    
-    detect(image)
-    
-    # Poll the keyboard. If 'q' is pressed, exit the main loop.
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        cam_quit = 1
+def cardMode(image):
+    global imageGlobal
+    ranks = []
+    suits = []
+    t_end = time.time() + 1
+    while time.time() < t_end:
+        imageGlobal = videostream.read()
+        rank, suit = detect(imageGlobal)
+        ranks.append(rank)
+        suits.append(suit)
         
+    print(max(set(ranks), key=ranks.count), max(set(suits), key=suits.count))
+    return cardAllowed(max(set(ranks), key=ranks.count), max(set(suits), key=suits.count))
+    
+def cardAllowed(rank, suit):
+    nRank = 0
+    nSuit = 0
+    
+    if rank == 'Ace':
+        nRank = 1
+    elif rank == 'Two':
+        nRank = 2
+    elif rank == 'Three':
+        nRank = 3
+    elif rank == 'Four':
+        nRank = 4
+    elif rank == 'Five':
+        nRank = 5
+    elif rank == 'Six':
+        nRank = 6
+    elif rank == 'Seven':
+        nRank = 7
+    elif rank == 'Eight':
+        nRank = 8
+    elif rank == 'Nine':
+        nRank = 9
+    elif rank == 'Ten':
+        nRank = 10
+    elif rank == 'Jack':
+        nRank = 11
+    elif rank == 'Queen':
+        nRank = 12
+    elif rank == 'King':
+        nRank = 13
+        
+    if suit == 'Clubs':
+        nSuit = 0
+    elif suit == 'Hearts':
+        nSuit = 1
+    elif suit == 'Spades':
+        nSuit = 2
+    elif suit == 'Diamonds':
+        nSuit = 3
+    
+    card = nRank + nSuit*13
+    print(card)
+    
+    if card in cardsAllowed:
+        return False
+    
+    return True
+    
+def main ():
+    print("INIT")
+    global imageGlobal
+    cam_quit = 0 # Loop control variable
+    
+    
+    #with serial.Serial(serial_port, baud_rate) as ser:  
+        #listen_ard(ser)
+    
+
+    # Begin capturing frames
+    while cam_quit == 0:
+        imageGlobal = videostream.read()
+        cv2.imshow("image", imageGlobal)
+            
+        #cardMode(pre_proc)
+       
+            
+        # Poll the keyboard. If 'q' is pressed, exit the main loop.
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            cam_quit = 1
+        elif key == ord("d"):
+            cardMode(imageGlobal)
+                
+                
+if __name__ == "__main__":
+    main()
 
 # Close all windows and close the PiCamera video stream.
 cv2.destroyAllWindows()
